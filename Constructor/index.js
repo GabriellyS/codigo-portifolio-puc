cb4323a8a824f4d22d0fac16e6f2c359154cfaf2
Gabrielly dos Santos <santos.gabrielly14@gmail.com>
2024-05-05 10:57:57 -0300

    Atualizando o Constructor

--- Constructor/index.js
+++ Constructor/index.js
@@ -1,15 +1,18 @@
-function Pessoa(){
-    this.nome = "Joao"
-    this.anoNascimento = 1997
-    this.profissao = "Estudante" 
+function Pessoa(nome,anoNascimento,profissao){
+    this.nome = nome
+    this.anoNascimento = anoNascimento
+    this.profissao = profissao
     this.calculaIdade = function(){
         //New Date já é uma funcao do JS
         return new Date().getUTCFullYear() - this.anoNascimento
     }
 }
 
-const pessoa1 = new Pessoa
-console.log(pessoa1.calculaIdade())
+const pessoa1 = new Pessoa("Joao", 1997, "Estudante")
+console.log(pessoa1.nome + " tem " + pessoa1.calculaIdade() + " anos e esta na profissão de " + pessoa1.profissao)
 //alterando a idade da Pessoa 1
-pessoa1.anoNascimento = 1983
-console.log(pessoa1.calculaIdade())
+//pessoa1.anoNascimento = 1983
+//console.log(pessoa1.calculaIdade())
+
+const pessoa2 = new Pessoa("Maria", 1968, "Geóloga")
+console.log(pessoa2.nome + " tem " + pessoa2.calculaIdade()+ " anos e esta na profissão de " + pessoa2.profissao)
