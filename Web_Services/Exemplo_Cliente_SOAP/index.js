9190f337ebe70316f72eadc77c31098850beedc2
Gabrielly dos Santos <santos.gabrielly14@gmail.com>
2024-05-21 09:05:22 -0300

    Utilizando um Web service de uma calculadora

--- Web_Services/Exemplo_Cliente_SOAP/index.js
+++ Web_Services/Exemplo_Cliente_SOAP/index.js
@@ -2,5 +2,39 @@ const soap = require("soap")
 var url = 'http://www.dneonline.com/calculator.asmx?wsdl'
 
 soap.createClient(url, function(err, client){
-    console.log("Descrever o WSDL", client.describe().Calculator.CalculatorSoap)
+    //Para simplificar o WSDL e ver ele como se fosse JSON tira as barras da linha de baixo
+    //console.log("Descrever o WSDL", client.describe().Calculator.CalculatorSoap)
+
+    //err é erro
+    client.Add({intA: 70, intB: 2}, function(err, result){
+        if(err){
+            console.log(err)
+        }else{
+            console.log("A soma deu: ", result.AddResult)
+        }
+    })
+
+    client.Subtract({intA: 70, intB: 2}, function(err, result){
+        if(err){
+            console.log(err)
+        }else{
+            console.log("A subtração deu: ", result.SubtractResult)
+        }
+    })
+
+    client.Multiply({intA: 70, intB: 2}, function(err, result){
+        if(err){
+            console.log(err)
+        }else{
+            console.log("A multiplicação deu: ", result.MultiplyResult)
+        }
+    })
+
+    client.Divide({intA: 70, intB: 2}, function(err, result){
+        if(err){
+            console.log(err)
+        }else{
+            console.log("A divisão deu: ", result.DivideResult)
+        }
+    })
 })
